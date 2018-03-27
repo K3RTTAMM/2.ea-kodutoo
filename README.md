15fe3dc76e2d2fdb7d0928db017c0c637bb33dc1
Kert Tamm <31847373+K3RTTAMM@users.noreply.github.com>
2018-03-27 15:20:49 +0300

    changes to app.js and readme.md

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # klahvimine 
 
+Tauri Taevik, Kert Tamm, Tatjana Kuznetsova
+
+* Eesmärgiks on trükkida ekraanile ilmuvaid sõnu.
+* Iga õigesti trükitud tähe eest suureneb skoor ühe võrra ning iga vale tähe eest väheneb skoor ühe võrra.
+* Peale igat viiendat õigesti kirjutatud sõna lisandub skoorile boonus +10.
+
 * Mäng, mille eesmärk on võimalikult kiiresti ekraanile tekkivaid sõnu ära trükkida
 * Sõnad võetud [Eesti Keele Instituudi lehelt](http://www.eki.ee/tarkvara/wordlist/), [lemmad2013](http://www.eki.ee/tarkvara/wordlist/lemmad2013.txt)
+
+Skeem on hetkel veel pooleldi valmis, lisame täiendusena.
--- app.js
+++ app.js
@@ -21,211 +21,197 @@ const TYPER = function () {
 	this.playerName = document.getElementById("playerNameText").value
   
 	this.init()
-  }
+}
   
   window.TYPER = TYPER
   
-  TYPER.prototype = {
-	  init: function () {
-		  console.log("TYPER.prototype")
-		  this.canvas = document.getElementsByTagName('canvas')[0]
-		  this.ctx = this.canvas.getContext('2d')
-		  this.canvas.style.width = this.WIDTH + 'px'
-		  this.canvas.style.height = this.HEIGHT + 'px'
-		  this.canvas.width = this.WIDTH * 2
-		  this.canvas.height = this.HEIGHT * 2
-		  this.loadWords()
-	  },
+TYPER.prototype = {
+	init: function () {
+		console.log("TYPER.prototype")
+		this.canvas = document.getElementsByTagName('canvas')[0]
+		this.ctx = this.canvas.getContext('2d')
+		this.canvas.style.width = this.WIDTH + 'px'
+		this.canvas.style.height = this.HEIGHT + 'px'
+		this.canvas.width = this.WIDTH * 2
+		this.canvas.height = this.HEIGHT * 2
+		this.loadWords()
+	},
   
-  loadWords: function () {
-	  console.log("loadWords: function")
-	  const xmlhttp = new XMLHttpRequest()
-	  xmlhttp.onreadystatechange = function () {
-		  console.log("xmlhttp.onreadystatechange function")
-			  if (xmlhttp.readyState === 4 && (xmlhttp.status === 200 || xmlhttp.status === 0)) {
-				  const response = xmlhttp.responseText
-				  const wordsFromFile = response.split('\n')
-				  typer.words = structureArrayByWordLength(wordsFromFile)
-				  typer.start()
-		  }
-	  }   
-	  xmlhttp.open('GET', './lemmad2013.txt', true)
-	  xmlhttp.send()
-  },
+loadWords: function () {
+	console.log("loadWords: function")
+	const xmlhttp = new XMLHttpRequest()
+	xmlhttp.onreadystatechange = function () {
+		console.log("xmlhttp.onreadystatechange function")
+			if (xmlhttp.readyState === 4 && (xmlhttp.status === 200 || xmlhttp.status === 0)) {
+				const response = xmlhttp.responseText
+				const wordsFromFile = response.split('\n')
+				typer.words = structureArrayByWordLength(wordsFromFile)
+				typer.start()
+			}
+	}   
+	xmlhttp.open('GET', './lemmad2013.txt', true)
+	xmlhttp.send()
+},
   
-  start: function () {
-	  console.log("start: function")
-	  this.generateWord()
-	  this.word.Draw()
-	  window.addEventListener('keypress', this.keyPressed.bind(this))
-  },
+start: function () {
+	console.log("start: function")
+	this.generateWord()
+	this.word.Draw()
+	window.addEventListener('keypress', this.keyPressed.bind(this))
+},
   
-  generateWord: function () {
-	  console.log("generateWord function")
-	  const generatedWordLength = this.wordMinLength + parseInt(this.guessedWords / 2)
-	  const randomIndex = (Math.random() * (this.words[generatedWordLength].length - 1)).toFixed()
-	  const wordFromArray = this.words[generatedWordLength][randomIndex]
-	  this.word = new Word(wordFromArray, this.canvas, this.ctx)
-  },
+generateWord: function () {
+	console.log("generateWord function")
+	const generatedWordLength = this.wordMinLength + parseInt(this.guessedWords / 2)
+	const randomIndex = (Math.random() * (this.words[generatedWordLength].length - 1)).toFixed()
+	const wordFromArray = this.words[generatedWordLength][randomIndex]
+	this.word = new Word(wordFromArray, this.canvas, this.ctx)
+},
   
-	  keyPressed: function (event) {
-		  console.log("keypressed: function")
-		  const letter = String.fromCharCode(event.which)
+	keyPressed: function (event) {
+		console.log("keypressed: function")
+		const letter = String.fromCharCode(event.which)
   
 	  
-	  if (letter === this.word.left.charAt(0)) {
-		  this.word.removeFirstLetter()
-		  this.score += 1 // skoor +1 kui on õige täht
-		  console.log(this.score)
-		  updateScore(this.score)
+	if (letter === this.word.left.charAt(0)) {
+		this.word.removeFirstLetter()
+		this.score += 1 // skoor +1 kui on õige täht
+		console.log(this.score)
+		updateScore(this.score)
   
-		  document.body.style.background = "white" // õige täht = background white	  
+		document.body.style.background = "white" // õige täht = background white	  
 		
-		  if (this.word.left.length === 0) {
-		  this.guessedWords += 1
-		  if(this.guessedWords%5 ===0){ // peale viit õigesti arvatud sõna lisandub skoorile boonus +10
-			  this.score+=10
-		  }
-		  console.log(this.score)
-		  updateScore(this.score)
-		  this.generateWord()
-		  
-		  
-  
+		if (this.word.left.length === 0) {
+		this.guessedWords += 1
+		if(this.guessedWords%5 ===0){ // peale viit õigesti arvatud sõna lisandub skoorile boonus +10
+			this.score+=10
 		}
-  
+		console.log(this.score)
+		updateScore(this.score)
+		this.generateWord()
+		} 
 		this.word.Draw()
-	  }
-	  else{
-		  document.body.style.background = "red" // vale täht = background red
-		  this.score -=1
-		  console.log(this.score)
-		  updateScore(this.score)
-		  
-	  }
 	}
-  }
+	else{
+		document.body.style.background = "red" // vale täht = background red
+		this.score -=1
+		console.log(this.score)
+		updateScore(this.score)
+		  
+		}
+	}
+}
   
   /* WORD */
-  const Word = function (word, canvas, ctx) {
-	  console.log("const Word function")
-	  this.word = word
-	  this.left = this.word
-	  this.canvas = canvas
-	  this.ctx = ctx
-  }
+const Word = function (word, canvas, ctx) {
+	console.log("const Word function")
+	this.word = word
+	this.left = this.word
+	this.canvas = canvas
+	this.ctx = ctx
+}
   
-  Word.prototype = {
-	  Draw: function () {
-	  console.log("Draw: function")
-	  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+Word.prototype = {
+	Draw: function () {
+	console.log("Draw: function")
+	this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
   
-	  this.ctx.textAlign = 'center'
-	  this.ctx.font = '140px Courier'
-	  this.ctx.fillText(this.left, this.canvas.width / 2, this.canvas.height / 2)
-	  },
+	this.ctx.textAlign = 'center'
+	this.ctx.font = '140px Courier'
+	this.ctx.fillText(this.left, this.canvas.width / 2, this.canvas.height / 2)
+	},
   
-	  removeFirstLetter: function () {
-		  console.log("removeFirstLetter function")
-		  this.left = this.left.slice(1)
-	  }
-  }
+	removeFirstLetter: function () {
+		console.log("removeFirstLetter function")
+		this.left = this.left.slice(1)
+	}
+}
   
   /* HELPERS */
   function structureArrayByWordLength (words) {
 	console.log("structureArrayByWordLength function")
 	let tempArray = []
   
-	  for (let i = 0; i < words.length; i++) {
-		  const wordLength = words[i].length
-		  if (tempArray[wordLength] === undefined)tempArray[wordLength] = []
+	for (let i = 0; i < words.length; i++) {
+		const wordLength = words[i].length
+		if (tempArray[wordLength] === undefined)tempArray[wordLength] = []
+			tempArray[wordLength].push(words[i])
+	}
+		return tempArray
+}
   
-		  tempArray[wordLength].push(words[i])
-	  }
-	  return tempArray
-  }
-  
-  function checkNameInput() {
-	  if (document.getElementById("playerNameText").value != ""){
-		  startGame()
-	  } else {
-		  document.getElementById("playerNameText").value= "Anonymous"
-		  startGame()
-	  }
+function checkNameInput() {
+	if (document.getElementById("playerNameText").value != ""){
+		startGame()
+	} else {
+		document.getElementById("playerNameText").value= "Anonymous"
+		startGame()
+	}
 	  
-  }
+}
   
- 
-  
-  function startGame(){
-	  document.getElementById("gameStartDiv").innerHTML="<canvas></canvas>"
-	  showHide("gameMenu");
-	  showHide("topBar")
-	  const typer = new TYPER()
-	  window.typer = typer
+function startGame(){
+	document.getElementById("gameStartDiv").innerHTML="<canvas></canvas>"
+	showHide("gameMenu")
+	showHide("topBar")
+	const typer = new TYPER()
+	window.typer = typer
 		  
-	  let display = document.querySelector('#time');
-	  let time = 10 // timeri aeg sekundites
-	  let duration
-	  startTimer(time, display);
-  }
+	let display = document.querySelector('#time');
+	let time = 10 // timeri aeg sekundites
+	let duration
+	startTimer(time, display);
+}
   
-  function newGame(){
+function newGame(){
+	const typer = new TYPER()
+	window.typer = typer
+	typer.guessedWords = 0
+	typer.guessedLetters = 0
+	typer.score = 0
+	document.getElementById("score").innerHTML=""
+	typer.generateWord()
+	typer.word.Draw()
+	startTimer(10, display)
+}
   
-	  const typer = new TYPER()
-	  window.typer = typer
-	  typer.guessedWords = 0
-	  typer.guessedLetters = 0
-	  typer.score = 0
-	  document.getElementById("score").innerHTML=""
-	  typer.generateWord()
-	  typer.word.Draw()
-	  startTimer(10, display)
-  }
+window.onload = function(){
+	showHide("topBar")
+	showHide("scores")
+	showHide("menuButton")
+	top10Players()  
+}
   
-  window.onload = function(){
-	  showHide("topBar")
-	  showHide("scores")
-	  showHide("menuButton")
-	  top10Players()
-	  
-  }
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
+function showHide(divID) {
+	let x = document.getElementById(divID);
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
   
-  //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
-  function showHide(divID) {
-	  var x = document.getElementById(divID);
-	  if (x.style.display === "none") {
-		  x.style.display = "block";
-	  } else {
-		  x.style.display = "none";
-	  }
-  }
-  
-  function updateScore(score){
-	  document.getElementById("score").innerHTML = score+ " punkti (" + typer.guessedWords + " sõna kirjutatud)"
-  }
+	function updateScore(score){
+		document.getElementById("score").innerHTML = score+ " punkti (" + typer.guessedWords + " sõna kirjutatud)"
+	}
   
   
-  function saveScore(){
+function saveScore(){ 
+	let allPlayerData = []
+	let singlePlayerData = {playerName: typer.playerName, playerScore: typer.score}
+	let storageData = null
   
-	  let allPlayerData = []
-		  let singlePlayerData = {playerName: typer.playerName, playerScore: typer.score}
-		  let storageData = null
-  
-		  if(localStorage.getItem("allPlayerData")){
-			  storageData = JSON.parse(localStorage.getItem("allPlayerData"))
-			  if(allPlayerData = storageData){
-				  allPlayerData = storageData
-			  }
-  
-		  }
+		if(localStorage.getItem("allPlayerData")){
+			storageData = JSON.parse(localStorage.getItem("allPlayerData"))
+			if(allPlayerData = storageData){
+				allPlayerData = storageData
+			}
+		}
   
 		  allPlayerData.push(singlePlayerData)
 		  localStorage.setItem("allPlayerData", JSON.stringify(allPlayerData))
-   }
-
-
-
+}
 
 //http://www.internet-technologies.ru/articles/sortirovka-massivov-v-javascript.html
 function sortScores(i, ii) { // Убывание
@@ -239,56 +225,49 @@ function sortScores(i, ii) { // Убывание
 }
 
 function top10Players() {
-	var allPlayerData = JSON.parse(localStorage.getItem("allPlayerData"));
-	var content = document.getElementsByClassName('top10players')[0];
+	let allPlayerData = JSON.parse(localStorage.getItem("allPlayerData"));
+	let content = document.getElementsByClassName('top10players')[0];
 
 	allPlayerData.sort(sortScores);
 
 	for (i = 0; i < 10; i++) {
 		content.innerHTML += "<li>" + "Name: " + allPlayerData[i].playerName + " Score: " + allPlayerData[i].playerScore + "</li>";
 	}
-
-	
-
 }
 
 function showScores(){
 	showHide("scores")
 	showHide("playerName")
 }
-
   
-  //https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
-  function startTimer(duration, display) {
-	var timer = duration, minutes, seconds
+// https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
+function startTimer(duration, display) {
+let timer = duration, minutes, seconds
 	setInterval(function () {
-		  minutes = parseInt(timer / 60, 10)
-		  seconds = parseInt(timer % 60, 10)
+		minutes = parseInt(timer / 60, 10)
+		seconds = parseInt(timer % 60, 10)
   
-		  minutes = minutes < 10 ? "0" + minutes : minutes
-		  seconds = seconds < 10 ? "0" + seconds : seconds
+		minutes = minutes < 10 ? "0" + minutes : minutes
+		seconds = seconds < 10 ? "0" + seconds : seconds
   
-		  display.textContent = minutes + ":" + seconds
+		display.textContent = minutes + ":" + seconds
   
-	  if (--timer < 0) {
+		if (--timer < 0) {	  
+			let again = confirm("Mäng läbi! \nSinu skoor on: " + typer.score + " \nMängi uuesti?")
 		  
-  
-		  
-		  let again = confirm("Mäng läbi! \nSinu skoor on: " + typer.score + " \nMängi uuesti?")
-		  
-		  if(again === true){
-			  timer = duration
-			  saveScore()
-			  newGame()
+			if(again === true){
+				timer = duration
+				saveScore()
+				newGame()
 			  
-		  }else if( again=== false){
-			  timer = duration
-			  saveScore()
-			  window.location.href = "index.html"
-		  }
-		  
+			}else if( again=== false){
+				timer = duration
+				saveScore()
+				window.location.href = "index.html"
+			}		  
 		}
-	   }, 1000);	
-  }
-
-  //TOP10//
+	}, 1000);	
+}
+  
+// Kasutatud abimaterjalina mõningaid Helpers funktsioone järgmisest repositooriumist.
+// https://github.com/eesrakenduste-arendamine-2018k/2.ea-kodutoo/pull/1
